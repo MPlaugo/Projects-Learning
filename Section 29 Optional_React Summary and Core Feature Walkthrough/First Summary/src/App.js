d30a6b881837292b9376291b8983b05310c6bbd7
MPlaugo <mplaugo98@gmail.com>
2022-02-24 00:11:10 +0100

    Lesson 29 part 1

---
+++ Section 29 Optional_React Summary and Core Feature Walkthrough/First Summary/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>Todoss</h1>
+      <Todo text="Learn React" />
+      <Todo text="Expert React" />
+      <Todo text="MasterReact" />
+    </div>
+  );
+}
+
+export default App;
