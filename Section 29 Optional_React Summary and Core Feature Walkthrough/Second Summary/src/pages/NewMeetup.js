ccf9f5b4f7984568967d03414ef12f710444db0e
MPlaugo <mplaugo98@gmail.com>
2022-02-24 00:14:43 +0100

    Summary 29 part 2

---
+++ Section 29 Optional_React Summary and Core Feature Walkthrough/Second Summary/src/pages/NewMeetup.js
@@ -0,0 +1,30 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const history = useHistory();
+
+  function onAddMeetupHandler(meetupData) {
+    fetch(
+      "https://react-spa-a8b1b-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
